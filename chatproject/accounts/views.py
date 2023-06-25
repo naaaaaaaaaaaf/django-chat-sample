9d2bb2cc1bae4db46c2694e32bd57cbb2718c3d0
Takato Ueno <naf@auriga.dev>
2023-06-25 16:15:37 +0900

    アカウント系の機能作成

--- chatproject/accounts/views.py
+++ chatproject/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login
+from .forms import AccountCreationForm
 
-# Create your views here.
+class AccountRegisterView(CreateView):
+    form_class = AccountCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
